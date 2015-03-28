48670df5a57f5494c119fba4132ea4e6c18bd8af
Daniel Segesdi <segesdi.dani@gmail.com>
2015-03-28 20:37:26 +0100

    Basic REST sevice for uploading and downloading csv files.

---
+++ BatchService/IWebService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BatchService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IWebService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/ContactInfo/{id}")]
+        Stream GetContactInfo(string id);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = WebRequestMethods.Http.Post,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/UploadCsv")]
+        string UploadCsv(Stream stream);
+    }
+}
