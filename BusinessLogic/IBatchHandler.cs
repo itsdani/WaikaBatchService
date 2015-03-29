b416cd6964f8d87eee8f8ab162ed4b508f42a690
Daniel Segesdi <segesdi.dani@gmail.com>
2015-03-29 10:57:31 +0200

    Interface for the business logic (IBatchHandler)

---
+++ BusinessLogic/IBatchHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public interface IBatchHandler
+    {
+        string AddBatch(Stream batchStream, string apiKey);
+        Stream GetResults(string id);
+    }
+}
