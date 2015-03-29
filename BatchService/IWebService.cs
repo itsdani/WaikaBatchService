ce2d9335403944556efdc2ea1b097d8e7520966c
Daniel Segesdi <segesdi.dani@gmail.com>
2015-03-29 11:07:20 +0200

    Removed unused imports

--- BatchService/IWebService.cs
+++ BatchService/IWebService.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
-using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
-using System.Text;
 
 namespace BatchService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IWebService
     {
