6561df4b0c69209fc63a8a7851cd968a9f575ade
Daniel Segesdi <segesdi.dani@gmail.com>
2015-04-01 16:28:11 +0200

    Added a project containing common model classes

---
+++ Model/ContactBatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Waika.Model
+{
+    public class ContactBatch
+    {
+        private List<ContactInfo> _contacts;
+
+        public List<ContactInfo> Contacts
+        {
+            get
+            {
+                if (_contacts == null)
+                {
+                    _contacts = new List<ContactInfo>();
+                }
+                return _contacts;
+            }
+            set { _contacts = value; }
+        }
+        public string ApiKey { get; set; }
+        public Guid Id { get; private set; }
+
+        public ContactBatch(Guid id, string apiKey)
+        {
+            Id = id;
+            ApiKey = apiKey;
+        }
+    }
+}
---
+++ Model/ContactInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waika.Model
+{
+    public class ContactInfo
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
