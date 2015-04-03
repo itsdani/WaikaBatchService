c37f64767137d6bde4ae6d6dd240d82710b977cd
Daniel Segesdi <segesdi.dani@gmail.com>
2015-04-03 10:49:45 +0200

    Added client for white pages service

---
+++ ContactInfoClient/IContactInfoClient.cs
@@ -0,0 +1,9 @@
+﻿using Waika.Model;
+
+namespace Waika.ContactInfoClient
+{
+    public interface IContactInfoClient
+    {
+        ContactInfo FillOutContactInfo(ContactInfo requestInfo, string apiKey);
+    }
+}
---
+++ ContactInfoClient/WhitePagesClient.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using Waika.Model;
+
+namespace Waika.ContactInfoClient
+{
+    public class WhitePagesClient : IContactInfoClient
+    {
+        private static string PersonSearchURI = "https://proapi.whitepages.com/2.1/";
+        public ContactInfo FillOutContactInfo(ContactInfo requestInfo, string apiKey)
+        {
+            
+            var client = new RestClient(PersonSearchURI);
+            var request = new RestRequest("person.json", Method.GET);
+            request.AddParameter("api_key", apiKey);
+            request.AddParameter("name", requestInfo.Name);
+            request.AddParameter("address", requestInfo.Address);
+
+
+            var response = client.Execute(request);
+            JObject jObject = JObject.Parse(response.Content);
+            dynamic data = jObject;
+            
+
+
+
+            return requestInfo;
+        }
+    }
+}
